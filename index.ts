f8a5c589133f3cec34b80bc9353949765a6466dc
RizwanBaig1 <rizwanbaig24400@gmail.com>
2024-05-28 01:27:52 +0500

    currancy canvater

---
+++ index.ts
@@ -0,0 +1,41 @@
+import inqurier from "inquirer"  
+
+const currency:any = {
+    USD:1,// BASE CURRENCY
+    EUR:0.91,
+    GBP:0.76,
+    INR:74.54,
+    PKR:280,
+};
+
+let user_answer:any = await inqurier.prompt([
+    {
+   name: "from",
+   message:"Enter from Currency",
+   type:"list",
+   choices: ["USD","EUR","GBP","INR","PKR"]
+},
+{
+    name: "to",
+    message:"Enter to Currency",
+    type: "list",
+    choices: ["USD","EUR","GBP","INR","PKR"]
+},
+{
+    name: "amount",
+    messsage: "Enter your amount",
+    type: "number"
+
+}
+] 
+)
+let fromAmount = currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount/ fromAmount //USD BASE CURRENCY
+let convertAmount =baseAmount*toAmount
+console.log(convertAmount);
+
+
+
+    
